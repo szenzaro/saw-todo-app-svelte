9bf6865a159693ba6c8e19781bdedfc7f2b77b06
Simone Zenzaro <s.zenzaro@gmail.com>
2023-04-19 13:18:10 +0200

    Add realtime updates

--- src/dbStore.ts
+++ src/dbStore.ts
@@ -1,5 +1,5 @@
 import { app } from "./firebaseConf";
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, setDoc, updateDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore, onSnapshot, orderBy, query, updateDoc } from 'firebase/firestore';
 import type { Todo, TodoRequest } from "./todoStore";
 import { writable } from "svelte/store";
 
@@ -8,8 +8,14 @@ const db = getFirestore(app);
 function createFirebaseStore() {
     const { subscribe, set, update } = writable<Todo[]>([]);
 
+    const todosQuery = query(collection(db, 'todos'), orderBy("date"));
+    const unsubscribe = onSnapshot(todosQuery, (querySnapshot) => {
+        const newData = querySnapshot.docs.map((d) => ({ id: d.id, ...d.data() } as Todo));
+        set(newData);
+    });
+
     const getTodos = async () => {
-        const res = await getDocs(collection(db, 'todos'));
+        const res = await getDocs(todosQuery);
         set(res.docs.map((d) => ({ id: d.id, ...d.data() } as Todo)));
     }
 
@@ -19,10 +25,8 @@ function createFirebaseStore() {
             state: 'ongoing',
             date: new Date(),
         };
-
         try {
-            const todoRef = await addDoc(collection(db, 'todos'), todoRequest);
-            update(ts => [...ts, { id: todoRef.id, ...todoRequest }]);
+            await addDoc(collection(db, 'todos'), todoRequest);
         } catch (e) {
             console.error(`Error adding ${todoRequest}`, e);
         }
@@ -32,7 +36,6 @@ function createFirebaseStore() {
         const { id, ...tr } = t;
         try {
             await updateDoc(doc(db, 'todos', id), tr);
-            update(ts => ts.map(v => v.id === t.id ? t : v));
         } catch (e) {
             console.error(`error updating ${t}`);
         }
@@ -40,7 +43,6 @@ function createFirebaseStore() {
     const deleteTodo = async (id: string) => {
         try {
             await deleteDoc(doc(db, 'todos', id));
-            update(ts => ts.filter(t => id !== t.id));
         } catch (e) {
             console.error(`error deleting ${id}`);
         }
